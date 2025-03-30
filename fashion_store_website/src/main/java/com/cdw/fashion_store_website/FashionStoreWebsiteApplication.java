eabbe436f3e07a6ed62441a783d12f5f5aa8f3e1
tranhoainam <21130450@st.hcmuaf.edu.vn>
2025-03-30 07:42:06 +0700

    Create project Spring Boot MVC

---
+++ fashion_store_website/src/main/java/com/cdw/fashion_store_website/FashionStoreWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.cdw.fashion_store_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionStoreWebsiteApplication.class, args);
+    }
+
+}
