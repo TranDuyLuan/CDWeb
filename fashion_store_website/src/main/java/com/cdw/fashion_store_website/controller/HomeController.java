475f9be045c2ad952ab223b311d7c439e6f2be8e
luan <20130070@st.hcmuaf.edu.vn>
2025-04-07 20:54:09 +0700

    mvc

---
+++ fashion_store_website/src/main/java/com/cdw/fashion_store_website/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.cdw.fashion_store_website.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Fashion Store Website is running!";
+    }
+}
